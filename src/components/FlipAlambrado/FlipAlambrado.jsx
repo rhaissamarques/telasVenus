d7d63b5511925c0ccdcac051ca20e51d87c5c200
Rhaissa Marques <rhaissa.marques@gmail.com>
2025-06-17 10:58:34 -0300

    update: flip pages alambrado and pinteiro

--- src/components/FlipAlambrado/FlipAlambrado.jsx
+++ src/components/FlipAlambrado/FlipAlambrado.jsx
@@ -1,15 +1,48 @@
-/*
-We're constantly improving the code you see. 
-Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
-*/
-
-import React from "react";
+import { useReducer } from "react";
 import "./style.css";
 
-export const FlipAlambrado = ({ className }) => {
+export const FlipAlambrado = ({ property1, className }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    property1: property1 || "component-2",
+  });
+
   return (
-    <div className={`flip-alambrado ${className}`}>
-      <div className="back-side-alambrado" />
+    <div
+      className={`flip-alambrado property-1-0-${state.property1} ${className}`}
+      onMouseEnter={() => {
+        dispatch("mouse_enter");
+      }}
+      onMouseLeave={() => {
+        dispatch("mouse_leave");
+      }}
+    >
+      <div className="back-side-alambrado">
+        {state.property1 === "component-2" && (
+          <img
+            className="image-3"
+            alt="Image"
+            src="src\components\images\flipAlambrado1.svg"
+          />
+        )}
+      </div>
     </div>
   );
 };
+
+function reducer(state, action) {
+  switch (action) {
+    case "mouse_enter":
+      return {
+        ...state,
+        property1: "component-1",
+      };
+
+    case "mouse_leave":
+      return {
+        ...state,
+        property1: "component-2",
+      };
+  }
+
+  return state;
+}
--- src/components/FlipPinteiro/FlipPinteiro.jsx
+++ src/components/FlipPinteiro/FlipPinteiro.jsx
@@ -1,9 +1,3 @@
-/*
-We're constantly improving the code you see. 
-Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
-*/
-
-import React from "react";
 import { useReducer } from "react";
 import "./style.css";
 
@@ -27,7 +21,7 @@ export const FlipPinteiro = ({ property1, className }) => {
           <img
             className="image-3"
             alt="Image"
-            src="https://c.animaapp.com/VEoDEv2v/img/image-4-1@2x.png"
+            src="src\components\images\flipPinteiro1.svg"
           />
         )}
       </div>
