de144d5c80f7ad934c3b123dc9517c06fd0feace
Rhaissa Marques <rhaissa.marques@gmail.com>
2025-06-17 14:16:52 -0300

    update: flip page ondulada

--- src/components/FlipOndulada/FlipOndulada.jsx
+++ src/components/FlipOndulada/FlipOndulada.jsx
@@ -1,15 +1,57 @@
-/*
-We're constantly improving the code you see. 
-Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
-*/
+// export const FlipOndulada = ({ className }) => {
+//   return (
+//     <div className={`flip-ondulada ${className}`}>
+//       <div className="back-side-ondulada" />
+//     </div>
+//   );
+// };
 
-import React from "react";
+
+import { useReducer } from "react";
 import "./style.css";
 
-export const FlipOndulada = ({ className }) => {
+export const FlipOndulada = ({ property1, className }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    property1: property1 || "component-2",
+  });
+
   return (
-    <div className={`flip-ondulada ${className}`}>
-      <div className="back-side-ondulada" />
+    <div
+      className={`flip-ondulada property-1-0-${state.property1} ${className}`}
+      onMouseEnter={() => {
+        dispatch("mouse_enter");
+      }}
+      onMouseLeave={() => {
+        dispatch("mouse_leave");
+      }}
+    >
+      <div className="back-side-ondulada">
+        {state.property1 === "component-2" && (
+          <img
+            className="image-3"
+            alt="Image"
+            src="src\components\images\flipOndulada1.svg"
+          />
+        )}
+      </div>
     </div>
   );
 };
+
+function reducer(state, action) {
+  switch (action) {
+    case "mouse_enter":
+      return {
+        ...state,
+        property1: "component-1",
+      };
+
+    case "mouse_leave":
+      return {
+        ...state,
+        property1: "component-2",
+      };
+  }
+
+  return state;
+}
