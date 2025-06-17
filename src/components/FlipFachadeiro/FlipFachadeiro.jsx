fc66507ebce69faa35b92f4789b4d28d44c184f8
Rhaissa Marques <rhaissa.marques@gmail.com>
2025-06-17 15:13:27 -0300

    update: flip page fachadeiro

--- src/components/FlipFachadeiro/FlipFachadeiro.jsx
+++ src/components/FlipFachadeiro/FlipFachadeiro.jsx
@@ -1,9 +1,48 @@
+import { useReducer } from "react";
 import "./styleFlipFachadeiro.css";
 
-export const FlipFachadeiro = ({ className, frameClassName }) => {
+export const FlipFachadeiro = ({ property1, className }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    property1: property1 || "component-2",
+  });
+
   return (
-    <div className={`flip-fachadeiro ${className}`}>
-      <div className={`frame ${frameClassName}`} />
+    <div
+      className={`flip-fachadeiro property-1-0-${state.property1} ${className}`}
+      onMouseEnter={() => {
+        dispatch("mouse_enter");
+      }}
+      onMouseLeave={() => {
+        dispatch("mouse_leave");
+      }}
+    >
+      <div className="back-side-fachadeiro">
+        {state.property1 === "component-2" && (
+          <img
+            className="image-3"
+            alt="Image"
+            src="src\components\images\flipFachadeiro1.svg"
+          />
+        )}
+      </div>
     </div>
   );
 };
+
+function reducer(state, action) {
+  switch (action) {
+    case "mouse_enter":
+      return {
+        ...state,
+        property1: "component-1",
+      };
+
+    case "mouse_leave":
+      return {
+        ...state,
+        property1: "component-2",
+      };
+  }
+
+  return state;
+}
