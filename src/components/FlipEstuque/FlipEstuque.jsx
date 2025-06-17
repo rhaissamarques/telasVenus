5d9913fb31b92a1a9e45aee6a3df0677c1aa0b48
Rhaissa Marques <rhaissa.marques@gmail.com>
2025-06-17 15:08:46 -0300

    update: flip page estuque

--- src/components/FlipEstuque/FlipEstuque.jsx
+++ src/components/FlipEstuque/FlipEstuque.jsx
@@ -1,15 +1,48 @@
-/*
-We're constantly improving the code you see. 
-Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
-*/
-
-import React from "react";
+import { useReducer } from "react";
 import "./style.css";
 
-export const FlipEstuque = ({ className }) => {
+export const FlipEstuque = ({ property1, className }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    property1: property1 || "component-2",
+  });
+
   return (
-    <div className={`flip-estuque ${className}`}>
-      <div className="div" />
+    <div
+      className={`flip-estuque property-1-0-${state.property1} ${className}`}
+      onMouseEnter={() => {
+        dispatch("mouse_enter");
+      }}
+      onMouseLeave={() => {
+        dispatch("mouse_leave");
+      }}
+    >
+      <div className="back-side-estuque">
+        {state.property1 === "component-2" && (
+          <img
+            className="image-3"
+            alt="Image"
+            src="src\components\images\flipEstuque1.svg"
+          />
+        )}
+      </div>
     </div>
   );
 };
+
+function reducer(state, action) {
+  switch (action) {
+    case "mouse_enter":
+      return {
+        ...state,
+        property1: "component-1",
+      };
+
+    case "mouse_leave":
+      return {
+        ...state,
+        property1: "component-2",
+      };
+  }
+
+  return state;
+}
